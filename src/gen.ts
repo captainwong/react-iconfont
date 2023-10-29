f4b1aeef9894bc9d01937fbeb15dd984017eaca3
i7-8700 <1281261856@qq.com>
2023-10-29 17:30:06 +0800

    update

--- src/gen.ts
+++ src/gen.ts
@@ -27,11 +27,7 @@ export const gen = (data: XmlData, config: Config) => {
     fs.writeFileSync(path.resolve(dir, 'main.tsx'), fs.readFileSync(path.resolve(__dirname, './templates/main.tsx.template'), 'utf-8'));
   }
   const template = fs.readFileSync(path.resolve(__dirname, './templates/icon.tsx.template'), 'utf-8');
-  let appTemplate = '';
-  if (config.gen_example) {
-    fs.readFileSync(path.resolve(__dirname, './templates/App.tsx.template'), 'utf-8');
-  }
-
+  
   data.svg.symbol.forEach((symbol) => {
     // console.log('symbol', symbol);
     const id = symbol.$.id;
@@ -42,6 +38,7 @@ export const gen = (data: XmlData, config: Config) => {
     let content = template.replace(/#Name#/g, fileName);
     content = content.replace(/#Size#/g, String(config.size));
     content = content.replace(/#Content#/g, genContent(symbol, 4));
+    content = content.replace(/\{size\}/g, `{size + '${config.unit}'}`);
     // console.log(content);
     fs.writeFileSync(path.resolve(dir, `iconfont/${fileName}.tsx`), content);
     imports.push(`import { ${fileName} } from './iconfont/${fileName}';`);
@@ -49,6 +46,7 @@ export const gen = (data: XmlData, config: Config) => {
   });
 
   if (config.gen_example) {
+    let appTemplate = fs.readFileSync(path.resolve(__dirname, './templates/App.tsx.template'), 'utf-8');
     appTemplate = appTemplate.replace(/#imports#/g, imports.join('\n'));
     appTemplate = appTemplate.replace(/#tr#/g, genTr(names));
     fs.writeFileSync(path.resolve(dir, 'App.tsx'), appTemplate);
@@ -66,24 +64,27 @@ const whiteSpace = (repeat: number) => {
 }
 
 const genContent = (data: XmlData['svg']['symbol'][number], indent: number) => {
-  let template = `\n${whiteSpace(indent)}<svg viewBox="${data.$.viewBox}" width={size} height={size} {...rest}>\n`;
+  const params = {
+    colorIndex: 0,
+    indent,
+  };
+  let template = `\n${whiteSpace(indent)}<svg viewBox="${data.$.viewBox}" width={size} height={size} style={style} {...rest}>\n`;
   data.path.forEach((path) => {
-    template += `${whiteSpace(indent + 2)}<path${genPathAttr(path, indent)}\n${whiteSpace(indent + 2)}/>\n`;
+    template += `${whiteSpace(indent + 2)}<path${genPathAttr(path, params)}\n${whiteSpace(indent + 2)}/>\n`;
   });
   template += `${whiteSpace(indent)}</svg>\n`;
   return template;
 }
 
-const genPathAttr = (path: XmlData['svg']['symbol'][number]['path'][number], indent: number) => {
+const genPathAttr = (path: XmlData['svg']['symbol'][number]['path'][number], params: {indent: number, colorIndex: number}) => {
   let attrs = '';
-  let colorIndex = 0;
   path.$.fill = path.$.fill || 'currentColor';
   Object.keys(path.$).forEach((attr) => {
     if (attr === 'fill') {
-      attrs += `\n${whiteSpace(indent + 4)}fill={getIconColor(color, ${colorIndex}, '${path.$.fill}')}`;
-      colorIndex += 1;
+      attrs += `\n${whiteSpace(params.indent + 4)}fill={getIconColor(color, ${params.colorIndex}, '${path.$.fill}')}`;
+      params.colorIndex += 1;
     } else {
-      attrs += `\n${whiteSpace(indent + 4)}${camelCase(attr)}="${path.$.d}"`;
+      attrs += `\n${whiteSpace(params.indent + 4)}${camelCase(attr)}="${path.$.d}"`;
     }
   });
   return attrs;
